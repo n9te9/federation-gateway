b6068133176c59456cc7c05d386feb6446249636
n9te9 <ai.kanaria.ai@gmail.com>
2026-02-02 14:07:39 +0900

    feat: error response

--- federation/executor/executor_test.go
+++ federation/executor/executor_test.go
@@ -364,17 +364,7 @@ func TestExecutor_Execute(t *testing.T) {
 			}
 
 			e := executor.NewExecutor(tt.httpClient, superGraph)
-			got, gotErr := e.Execute(t.Context(), tt.plan, tt.variables)
-
-			if gotErr == nil && tt.wantErr != nil || tt.wantErr == nil && gotErr != nil {
-				t.Fatalf("Executor.Execute() error = %v, wantErr %v", gotErr, tt.wantErr)
-			}
-
-			if gotErr != nil && tt.wantErr != nil {
-				if gotErr.Error() != tt.wantErr.Error() {
-					t.Fatalf("Executor.Execute() error = %v, wantErr %v", gotErr, tt.wantErr)
-				}
-			}
+			got := e.Execute(t.Context(), tt.plan, tt.variables)
 
 			if d := cmp.Diff(tt.want, got); d != "" {
 				t.Fatalf("Executor.Execute() diff: %s", d)
